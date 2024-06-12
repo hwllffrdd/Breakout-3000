a8dd5c62a87dab5b2bac4f0839942442929bc682
Ondrej Vit <ondrvit@gmail.com>
2024-06-12 21:52:30 +0200

    Added bricks and ball-brick colision detection

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
+from bricks import Brick
 import time
 
 screen = Screen()
@@ -13,6 +14,22 @@ screen.tracer(0)
 paddle = Paddle((0, -350))
 ball = Ball((0, -350))
 scoreboard = Scoreboard()
+
+brick_rows = 5
+brick_cols = 8
+brick_width = 60
+brick_height = 20
+brick_spacing_x = 10
+brick_spacing_y = 10
+
+bricks = []
+for y in range(brick_rows):
+    for x in range(brick_cols):
+        x_offset = (x * (brick_width + brick_spacing_x)) - (300 - (brick_width // 2))
+        if y % 2 == 0:
+            x_offset += brick_width // 2
+        y_offset = (y * (brick_height + brick_spacing_y)) + 50
+        bricks.append(Brick((x_offset, y_offset)))
 
 move_speed = 0.1
 
@@ -37,5 +54,11 @@ while game_is_on:
 
     if ball.ycor() < -375:
         game_is_on = False
+
+    for brick in bricks:
+        if ball.distance(brick) < 30:
+            brick.goto(1000, 1000)  # Move the brick off-screen (effectively "removing" it)
+            ball.bounce_y()
+            bricks.remove(brick)  # Remove brick from the list
 
 screen.exitonclick()
